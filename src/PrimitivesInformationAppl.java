551a7326e131edc55d1cfb30f6bc3c67af42fc69
NTsoi <czoj.n95@mail.ru>
2021-10-11 19:46:24 +0300

    adds switch by type

--- src/PrimitivesInformationAppl.java
+++ src/PrimitivesInformationAppl.java
@@ -6,7 +6,7 @@ public class PrimitivesInformationAppl {
 	public static void main(String[] args) {
 		Scanner scanner  = new Scanner(System.in); //input data from the console
 		while(true) {
-			System.out.println("enter integer primitive type (long, int, short, char, byte or exit");
+			System.out.println("enter integer primitive type (long, int, short, char, byte) or exit");
 			String line = scanner.nextLine();
 			if (line.equals("exit")) {
 				break;
@@ -21,18 +21,66 @@ public class PrimitivesInformationAppl {
 		// your homework should contain switch by the given type (long, int, short, char, byte)
 		//for default there should be out: Wrong primitive type
 		// for each type there should be an appropriate method of printing information of the type
+		
 		//for example, printCharInformation - method printing out the info about type "char"
-		char minValue = 1; 
-		char maxValue = 1;
-		int nBits = 1;
-		while(minValue > 0) {
-			minValue = (char) (minValue * 2);//compiler casts minValue to int, multiplies on 2 
-			nBits = nBits + 1;
+		switch(type) {
+		case "long":
+			long minValue = 1; 
+			long maxValue = 1;
+			int nBits = 1;
+			while(minValue > 0) {
+				minValue =  (minValue * 2);//compiler casts minValue to int, multiplies on 2 
+				nBits = nBits + 1;
+			}
+			maxValue =  (minValue - 1);
+			System.out.printf("minimal value is %d, maximal value is %d, number of bytes is %d\n", minValue, maxValue, nBits / 8);
+			break;
+		case "int":
+			int minValueInt = 1; 
+			int maxValueInt = 1;
+			int nBitsInt = 1;
+			while(minValueInt > 0) {
+				minValueInt =  (minValueInt * 2);//compiler casts minValue to int, multiplies on 2 
+				nBitsInt = nBitsInt + 1;
+			}
+			maxValueInt =  (minValueInt - 1);
+			System.out.printf("minimal value is %d, maximal value is %d, number of bytes is %d\n", minValueInt, maxValueInt, nBitsInt / 8);
+			break;
+		case "char":
+			char minValueChar = 1; 
+			char maxValueChar = 1;
+			int nBitsChar = 1;
+			while(minValueChar > 0) {
+				minValueChar = (char) (minValueChar * 2);//compiler casts minValue to int, multiplies on 2 
+				nBitsChar = nBitsChar + 1;
+			}
+			maxValueChar =  (char)(minValueChar - 1);
+			System.out.printf("minimal value is %d, maximal value is %d, number of bytes is %d\n",(int) minValueChar, (int) maxValueChar, nBitsChar / 8);
+			break;
+		case "short":
+			short minValueShort = 1; 
+			short maxValueShort = 1;
+			int nBitsShort = 1;
+			while(minValueShort > 0) {
+				minValueShort = (short) (minValueShort * 2);//compiler casts minValue to int, multiplies on 2 
+				nBitsShort = nBitsShort + 1;
+			}
+			maxValueShort =  (short)(minValueShort - 1);
+			System.out.printf("minimal value is %d, maximal value is %d, number of bytes is %d\n",(int) minValueShort, (int) maxValueShort, nBitsShort / 8);
+			break;
+		case "byte":
+			byte minValueByte = 1; 
+			byte maxValueByte = 1;
+			int nBitsByte = 1;
+			while(minValueByte > 0) {
+				minValueByte = (byte) (minValueByte * 2);//compiler casts minValue to int, multiplies on 2 
+				nBitsByte = nBitsByte + 1;
+			}
+			maxValueByte =  (byte)(minValueByte - 1);
+			System.out.printf("minimal value is %d, maximal value is %d, number of bytes is %d\n",(int) minValueByte, (int) maxValueByte, nBitsByte / 8);
+			break;
+			default:System.out.printf("Wrong type!!!");
 		}
-		maxValue = (char) (minValue - 1);
-		System.out.printf("minimal value is %d, maximal value is %d, number of bytes is %d\n", (int)minValue, (int)maxValue, nBits / 8);
-		
-		
 	}
 
 }
